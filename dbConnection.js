d6f18850fde5b7fde7ef5e6dc18b3a11d47d5274
Akshit Anand <22866916+akshit-anand@users.noreply.github.com>
2023-06-26 06:43:47 +0530

    added authentication and authorization with jwt and bcrypt

--- dbConnection.js
+++ dbConnection.js
@@ -2,10 +2,12 @@
 const mongoose = require("mongoose");
 
 const main = async () => {
-  await mongoose.connect(process.env.DB_URI);
-  console.log("Connected To Database");
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Connected To Database");
+  } catch (err) {
+    return console.log(err);
+  }
 };
-
-main().catch((err) => console.log(err));
 
 module.exports = main;
